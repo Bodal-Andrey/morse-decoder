45b67e06b92480e1d41391a5283ca74d048eee9a
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-08-13 18:16:31 +0500

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const getKeyByValue = (object, value) => {
+        return Object.keys(object).find(key => object[key] === value);
+    };
+    const arr = getKeyByValue(MORSE_TABLE, `m`);
+console.log(arr)
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        
+        if (arr[i] === `-`) {
+            newArr.push(11);
+        }
+        if (arr[i] === `.`) {
+        newArr.push(10);
+        }
+    }
+    const joinArr = newArr.join(``);
+    console.log(joinArr)
+    if (joinArr.length < 10) {
+        const bebe = joinArr.split(``);
+        console.log(bebe.length)
+        for (let i = 0; i < 10 - bebe.length; i) {
+            bebe.unshift(`0`);
+        }
+        return bebe.join(``);
+    }
+    return bebe;
 }
 
 module.exports = {
