fad5610efae524fe302f3a671d8b922880b4660e
Bodal-Andrey <a-v-bodal@yandex.ru>
2021-08-22 13:34:59 +0500

    feat: end morze-decoder task

--- src/index.js
+++ src/index.js
@@ -41,29 +41,43 @@ function decode(expr) {
     const getKeyByValue = (object, value) => {
         return Object.keys(object).find(key => object[key] === value);
     };
-    const arr = getKeyByValue(MORSE_TABLE, `m`);
-console.log(arr)
-    const newArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        
-        if (arr[i] === `-`) {
-            newArr.push(11);
+    const arr = expr.split(``).map(item => getKeyByValue(MORSE_TABLE, item));
+
+    const newArr = arr.map(item => item === undefined ? `**********` : item);
+
+    const result = newArr.map(item => {
+        const secondArr = [];
+        for (let i = 0; i < item.length; i++) {
+            if (item[i] === `*`) {
+                secondArr.push(`*`);
+            }
+
+            if (item[i] === `-`) {
+                secondArr.push(11);
+            }
+            if (item[i] === `.`) {
+                secondArr.push(10);
+            }
         }
-        if (arr[i] === `.`) {
-        newArr.push(10);
-        }
-    }
-    const joinArr = newArr.join(``);
-    console.log(joinArr)
-    if (joinArr.length < 10) {
-        const bebe = joinArr.split(``);
-        console.log(bebe.length)
-        for (let i = 0; i < 10 - bebe.length; i) {
-            bebe.unshift(`0`);
-        }
-        return bebe.join(``);
-    }
-    return bebe;
+        const thirdArr = secondArr.join(``);
+        let fourthArr = new Array(thirdArr);
+
+        const sixArr = fourthArr.map(item => {
+            if (item.length < 10) {
+                let seventhArr = item.split(``);
+
+                for (let i = 0; i < 10 - seventhArr.length; i) {
+                    seventhArr.unshift(`0`);
+                }
+                return seventhArr.join(``);
+            } else {
+                return item;
+            }
+
+        });
+        return sixArr;
+    });
+    return result.join(``);
 }
 
 module.exports = {
